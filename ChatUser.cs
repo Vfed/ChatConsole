f0e309791264acaf93d53eae8a625d982827fc23
Vfed <asser.282195@gmail.com>
2021-09-24 07:37:05 +0300

    Add project files.

---
+++ ChatUser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ChatConsole
+{
+    class ChatUser
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public List<ChatsList> Chats { get; set; }
+    }
+    class ChatsList
+    {
+        public Guid Id { get; set; }
+        public ChatUser ChatUser { get; set; }
+        public Chat Chat { get; set; }
+    }
+
+    class Chat 
+    {
+        public Guid ChatId { get; set; }
+        public string ChatName { get; set; }
+        public DateTime LastData { get; set; }
+        public List<ChatsList> ChatsList { get; set; }
+        public List<Message> Messages { get; set; }
+    }
+
+    class Message
+    {
+        public Guid Id { get; set; }
+        public Chat Chat { get; set; }
+        public string UserName { get; set; }
+        public string Massege { get; set; }
+        public DateTime CurrentTime { get; set; }
+    }
+}
---
+++ ChatUserDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatConsole
+{
+    public class ChatUserDto
+    {
+        public string Username { get; set; }
+    }
+}
